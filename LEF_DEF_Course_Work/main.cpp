8e187639818a28f4c83b9b4de4afa67b93880453
Razmik Karapetyan <karapetyanraz95@gmail.com>
2018-05-20 20:23:20 +0400

    Add threaded area calculation
    Signed-off-by: Razmik Karapetyan <karapetyanraz95@gmail.com>

--- LEF_DEF_Course_Work/main.cpp
+++ LEF_DEF_Course_Work/main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include "CCell.h"
 #include "CLefParser.h"
+#include <thread>
+#include <mutex>
+
+void area_calculator(const std::vector<CCell>& cells, 
+		     int start, int end, double& result)
+{
+	for (int i = start; i < end; ++i)
+		result += cells[i].area();
+}
 
 int main()
 {
 	// lef_example.lef
-	CLefParser parser("raz.txt");
+	// raz.txt
+	CLefParser parser("lef_example.lef");
 	std::vector<CCell> res = parser.retrieveCells();
-	std::cout << "cell count = " << parser.parsedCellCount() << std::endl;
-	
-	for (int i = 0; i < res.size(); ++i) 
-		std::cout << "Area = " << res[i].area() << std::endl;
+	int cell_count = parser.parsedCellCount();
+	double result = 0;
 
+	for (int i = 0; i < cell_count; i+=5)
+	{
+		int start = i;
+		int end = (i + 5 > cell_count) ? cell_count-1 : i+5;
+		std::thread th(area_calculator, std::cref(res), start, end,std::ref(result));
+		th.join();
+	}
+	std::cout << "cell count = " << cell_count << std::endl;
+
+	//Single Threaded Example
+	/*for (int i = 0; i < res.size(); ++i) 
+		result += res[i].area();*/
+
+	std::cout << " result = " << result << std::endl;
 	system("pause");
 	return 0;
 }
